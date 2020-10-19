29dd174ca10665d93a47eccbd625432930fc1c6d
mohammedelmala <mala_moh@msn.com>
2020-10-19 17:59:12 +0400

    create header and routers

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -2,18 +2,40 @@ import React from "react";
 
 import { BrowserRouter, Route } from "react-router-dom";
 
-const pageOne = () => {
-    return <div>Page One</div>
-}
+import StreamShow from "./streams/StreamShow";
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+
+import Header from "./Header";
+
+
 
 const App = () => {
+
     return (
-        <div>
+
+        <div className="ui container">
+
             <BrowserRouter>
-                <Route path="/" exact component={pageOne} />
+                <div>
+                    <Header></Header>
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/show" exact component={StreamShow} />
+                    <Route path="/streams/delete" exact component={StreamDelete} />
+                    <Route path="/streams/edit" exact component={StreamEdit} />
+
+                </div>
+
             </BrowserRouter>
+
         </div>
+
     )
+
+
 }
 
 
